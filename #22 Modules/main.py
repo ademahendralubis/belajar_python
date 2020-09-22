c377c580ea80adb0374cc26c362bda8c15263ca8
codingan <codinganai@gmail.com>
2020-09-22 13:30:50 +0700

    added new courses

---
+++ #22 Modules/main.py
@@ -0,0 +1,19 @@
+import themodule
+
+# call the function from the module
+themodule.something()
+
+# call the function and pass the argument
+#themodule.print_nama("budi")
+
+#create the module alias
+#import themodule as modul
+
+#call the function with the module alias
+#modul.something()
+
+#import only specific variable/function from the module
+#from themodule import kota, something
+
+# something()
+# print(kota)
---
+++ #22 Modules/themodule.py
@@ -0,0 +1,7 @@
+def something():
+    print("this is the module")
+
+def print_nama(name):
+    print("nama saya adalah {}".format(name))
+
+kota = ["Jakarta","Bandung","Bali"]
